9f47dcff04aedf34ce79ac24459fcaf51f3064a9
Christian <purin213@hotmail.com>
2023-09-26 21:54:26 +0900

    update README & format comment

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import threading
 import random
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# ポート番号がランダムで割り振られる、よっぽど運悪くなきゃダブらない…ハズ
 client.bind(("localhost", random.randint(8081, 9999)))
 
 name = input("NickName: ")
@@ -25,6 +26,7 @@ client.sendto(f"Nickname:{name}".encode(), ("localhost", 8080))
 while True:
     message = input("")
     if message == "exit":
+        # CI変更はないが、接続が切られるはず。
         exit()
     else:
         client.sendto(f"{name}: {message}".encode(), ("localhost", 8080))
