9e29f185043071b2397d4445947a9240c030f134
Morio Nakatani <m0rio.n0818ms5@gmail.com>
2023-09-28 13:49:55 +0900

    changed to class Style

--- client.py
+++ client.py
@@ -1,37 +1,45 @@
 import socket
 
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = "0.0.0.0"
-server_port = 9001
-
-
-address = ''
-port = 9050
-
-# UserName
-userName = input("Input your userName? : ")
-buserName = bytes(userName, "utf-8")
-# Message Send
-message = input("Input your message you want to sent? : ")
-bMessage =  bytes(message, 'utf-8')
-
-sock.bind((address,port))
-
-try:
-    print("Sending {!r}".format(bMessage))
-
-    sentuserName = sock.sendto(buserName, (server_address, server_port))
-    sentMessage = sock.sendto(bMessage, (server_address, server_port))
-    print("Send {} bytes ".format(sentuserName))
-    print("Send {} bytes ".format(sentMessage))
-
+class Client:
+    def __init__(self, address, port=8080, buffer=4096) -> None:
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__serverAddress = '0.0.0.0'
+        self.__serverPort = 9001
+        self.__address = address
+        self.__port = port
+        self.__buffer = buffer
     
-    # 応答を受信
-    print("Waiting receive")
-    data, server = sock.recvfrom(4096)
-    print("Recieved {!r}".format(data))
-    
-finally:
-    print("Closing socket")
-    sock.close()
+    def start(self):
+        print("Starting up on {} ".format(self.__serverPort))
+        self.__socket.bind((self.__address, self.__port))
+        self.sendandrecive()
+        
+    def sendandrecive(self):
+        try:
+            message = input("Input your message which you wanna send ? : ")
+            bMessage = bytes(message, "utf-8")
+            
+            # サーバーへデータを送信
+            sent = self.__socket.sendto(bMessage,(self.__serverAddress, self.__serverPort))
+            print('send {} bytes'.format(sent))
+            
+            # 応答を受信
+            print("Waiting to recive")
+            data, server = self.__socket.recvfrom(self.__buffer)
+            print("Recived {!r}".format(data))
+            
+        finally:
+            self.close() 
+        
+    def close(self):
+        print("Closing socket")
+        
+
+def main():
+    client_address = input("input your client address : ")
+    # port = 8080
+    client = Client(client_address)
+    client.start()
+     
+if __name__ == "__main__":
+    main()
